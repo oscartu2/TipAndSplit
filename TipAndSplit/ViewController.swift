943eaabb8c4b1bd0c6953685885e9e9740274bbb
Oscar Tu <o.car.w2@gmail.com>
2018-07-30 17:42:17 -0700

    Initial Commit

---
+++ TipAndSplit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipAndSplit
+//
+//  Created by Oscar on 7/30/18.
+//  Copyright © 2018 oscartu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
