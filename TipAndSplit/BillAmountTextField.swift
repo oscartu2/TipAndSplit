e66eb55daac364c45dc097599bc6cecb27324714
Oscar Tu <o.car.w2@gmail.com>
2018-09-12 00:16:36 -0700

    Update

--- TipAndSplit/BillAmountTextField.swift
+++ TipAndSplit/BillAmountTextField.swift
@@ -14,7 +14,7 @@ class BillAmountTextField : UITextField {
     
     // MARK: - Properties
     
-    var buttonTapAction: (() -> Void)?
+    var calculateButtonAction: (() -> Void)?
     
     // MARK: - View Lifecycle
     
@@ -25,7 +25,7 @@ class BillAmountTextField : UITextField {
         
         let leadingFlex = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
         let trailingFlex = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
-        let calculateButton = UIBarButtonItem(title: "Calculate Tip", style: .done, target: self, action: #selector(doneButtonTapped(_:)))
+        let calculateButton = UIBarButtonItem(title: "Calculate Tip", style: .done, target: self, action: #selector(calculateButtonTapped(_:)))
         toolbar.items = [leadingFlex, calculateButton, trailingFlex]
         
         // resizes toolbar
@@ -34,7 +34,7 @@ class BillAmountTextField : UITextField {
         self.inputAccessoryView = toolbar
     }
     
-    @objc private func doneButtonTapped(_ sender: UIBarButtonItem) {
-        buttonTapAction?()
+    @objc private func calculateButtonTapped(_ sender: UIBarButtonItem) {
+        calculateButtonAction?()
     }
 }
